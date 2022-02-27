14bef40fe742c940f7b373c4c661c7ae54fb2c09
jeferson-p <jeferson.p@univille.br>
2022-02-27 20:43:03 -0300

    ajuste foreach

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -42,42 +42,40 @@ class ProductStructure
         // Separando cor & tamanho;
         foreach ($products as $product) {
             $arr[] = explode('-', $product);
+        }
 
-            // Criando os índices "cor" e índices "tamanho ;
-            foreach ($arr as $key => $value) {
-                $indexColor[]   = $value[0];
-                $indexSize[]    = $value[1];
-            };
+        // Criando os índices "cor" e índices "tamanho ;
+        foreach ($arr as $key => $value) {
+            $indexColor[]   = $value[0];
+            $indexSize[]    = $value[1];
+        };
 
-            // Obtendo valores Únicos;
-            $indexColor = array_unique($indexColor);
-            $indexSize  = array_unique($indexSize);
+        // Obtendo valores Únicos;
+        $indexColor = array_unique($indexColor);
+        $indexSize  = array_unique($indexSize);
 
-            // Criando o formato da resposta;
-            foreach ($indexColor as $c) {
-                foreach ($indexSize as $s) {
-                    $count = 0;
-                    foreach ($products as $key) {
-                        if ($key === "$c-$s") {
-                            $count = ++$count;
-                        };
-                        $response["$c"]["$s"] = $count;
-                    }
-                }
-            }
-
-            // Removento Valores que não foram obtidos;
+        // Criando o formato da resposta;
+        foreach ($indexColor as $c) {
             foreach ($indexSize as $s) {
-                foreach ($response as $key => $value) {
-                    $element = $value[$s];
-                    if ($element === 0) {
-                        unset($response[$key][$s]);
-                    }
+                $count = 0;
+                foreach ($products as $key) {
+                    if ($key === "$c-$s") {
+                        $count = ++$count;
+                    };
+                    $response["$c"]["$s"] = $count;
                 }
             }
         }
 
-      
+        // Removento Valores que não foram obtidos;
+        foreach ($indexSize as $s) {
+            foreach ($response as $key => $value) {
+                $element = $value[$s];
+                if ($element === 0) {
+                    unset($response[$key][$s]);
+                }
+            }
+        }
 
         return $response;
     }
